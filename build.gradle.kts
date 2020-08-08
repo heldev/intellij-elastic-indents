78718e7a7dbc5246ddf69ac34f680ac83fba8601
heldev <heldevemail@gmail.com>
2020-08-08 02:01:07 -0400

    Clean up build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,7 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testCompile("junit", "junit", "4.12")
-}
-
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
     version = "2020.2"
-}
-tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
-    changeNotes("""
-      Add change notes here.<br>
-      <em>most HTML tags may be used</em>""")
 }
